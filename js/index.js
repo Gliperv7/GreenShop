9957ad1f52fda827a93381b2a2ce893ed1c32f5c
Gliperv7 <klimenkoarik4@gmail.com>
2025-09-16 18:16:20 +0300

    A modal login window has been added

--- js/index.js
+++ js/index.js
@@ -23,3 +23,30 @@ document.getElementById("my-modal").addEventListener('click', event => {
     if (event._isClickWithInModal) return;
     event.currentTarget.classList.remove('open');
 });
+
+
+
+document.getElementById("modal-login--open").addEventListener("click", function() {
+    document.getElementById("modal-login").classList.add("open")
+})
+
+
+document.getElementById("close--modal-btn-login").addEventListener("click", function() {
+    document.getElementById("modal-login").classList.remove("open")
+})
+
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("modal-login").classList.remove("open")
+    }
+});
+
+
+document.querySelector("#modal-login .modal__box-login").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+document.getElementById("modal-login").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+})
